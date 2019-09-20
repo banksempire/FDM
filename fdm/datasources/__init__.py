0567664e566af6937a76029086a265bc547c8051
zlf <zhulf4869@outlook.com>
2019-09-20 23:01:07 +0800

    MOD lazy import

--- fdm/datasources/__init__.py
+++ fdm/datasources/__init__.py
@@ -1,10 +1,18 @@
-from .tushare.model import Tushare as _tushare
-from .cleandata.model import CleanData as _cleandata
-from .wind.model import Wind as _wind
-from .tempdb import TempDB as _tempdb
+def Tushare():
+    from .tushare.model import Tushare
+    return Tushare()
 
 
-Tushare = _tushare()
-CleanData = _cleandata()
-Wind = _wind()
-TempDB = _tempdb()
+def CleanData():
+    from .cleandata.model import CleanData
+    return CleanData()
+
+
+def Wind():
+    from .wind.model import Wind
+    return Wind()
+
+
+def TempDB():
+    from .tempdb import TempDB
+    return TempDB()
