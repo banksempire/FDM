2d314c1b3d4a61b4129a40fc09772aea3c321058
Langford <bankszhu4869@gmail.com>
2019-09-20 16:39:26 +0800

    FIX tempdb class

--- fdm/datasources/tempdb.py
+++ fdm/datasources/tempdb.py
@@ -9,9 +9,6 @@ from .metaclass import _DbBase, ColInterface
 class TempDB(_DbBase):
     '''Database that holds temporary data.'''
 
-    def __init__(self, client: MongoClient, settingname='tempdb'):
-        super().__init__(client, settingname)
-
     def __getitem__(self, key: str):
         col = self.db[key]
         return ColInterface(col, self.setting['DBSetting'])
