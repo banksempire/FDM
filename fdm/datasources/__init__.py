dcc619f882c9757d94920dd993c262662407b44f
Langford <bankszhu4869@gmail.com>
2019-09-20 16:40:11 +0800

    CHANGE loading behavior

--- fdm/datasources/__init__.py
+++ fdm/datasources/__init__.py
@@ -1,4 +1,10 @@
-from .tushare.model import Tushare
-from .cleandata.model import CleanData
-from .wind.model import Wind
-from .tempdb import TempDB
+from .tushare.model import Tushare as _tushare
+from .cleandata.model import CleanData as _cleandata
+from .wind.model import Wind as _wind
+from .tempdb import TempDB as _tempdb
+
+
+Tushare = _tushare()
+CleanData = _cleandata()
+Wind = _wind()
+TempDB = _tempdb()
