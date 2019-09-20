92623122b1f35955b62d28792b7581c11cb75bde
Langford <bankszhu4869@gmail.com>
2019-09-20 17:04:05 +0800

    ADD update_all

--- fdm/utils/maintainer.py
+++ fdm/utils/maintainer.py
@@ -2,4 +2,7 @@ import fdm
 
 
 def update_all():
-    pass
+    fdm.Wind.edb().update()
+    fdm.Tushare.daily_adj().update()
+    fdm.Tushare.daily_basic().update()
+    fdm.Tushare.daily_price().update()
