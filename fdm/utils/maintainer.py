8ae20b1729ce7a67e3bc36addeb3e0e02c4c8112
Langford <bankszhu4869@gmail.com>
2019-09-20 16:40:34 +0800

    ADD maintainer.py

---
+++ fdm/utils/maintainer.py
@@ -0,0 +1,5 @@
+import fdm
+
+
+def update_all():
+    pass
--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import fdm
 
-edb = fdm.Wind().edb()
+edb = fdm.Wind.edb()
 # edb.interface.drop()
 data = edb.query(['M5567876', 'M5567877'], autoupdate=False)
 print(data.pivot('date', 'code', 'value'))
