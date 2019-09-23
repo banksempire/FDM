bedc810a4e3711befec713c7ea32875ce9c26978
Langford <bankszhu4869@gmail.com>
2019-09-23 14:56:43 +0800

    MOD update_all

--- fdm/utils/maintainer.py
+++ fdm/utils/maintainer.py
@@ -2,7 +2,7 @@ import fdm
 
 
 def update_all():
-    fdm.Wind.edb().update()
-    fdm.Tushare.daily_adj().update()
-    fdm.Tushare.daily_basic().update()
-    fdm.Tushare.daily_price().update()
+    fdm.Wind().edb().update()
+    fdm.Tushare().daily_adj().update()
+    fdm.Tushare().daily_basic().update()
+    fdm.Tushare().daily_price().update()
