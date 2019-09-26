80b54f80dc01108ad9170e0c2146416d966085b3
Langford <bankszhu4869@gmail.com>
2019-09-26 11:48:37 +0800

    ADD Bubbles and TimeBubbles data structure

--- fdm/_structure/bubbles.py
+++ fdm/_structure/bubbles.py
@@ -1,38 +1,53 @@
-class Bubble:
+from datetime import datetime
+from datetime import timedelta
+
+
+class TimeBubble:
     def __init__(self, lower, upper):
         if lower > upper:
             raise ValueError(
                 'Upper {} is less then lower {}.'.format(upper, lower))
         self.max = upper
         self.min = lower
-        self.mid = self.max + self.min
-        self.leg = self.max - self.min
+        self.mid = self.max.timestamp() + self.min.timestamp()
+        self.leg = self.max.timestamp() - self.min.timestamp()
 
     def __repr__(self):
-        return 'Bubble(lower={}, upper={})'.format(self.min, self.max)
+        start = self.min
+        end = self.max
+        return 'TimeBubble({}, {})'.format(repr(start), repr(end))
 
     def __str__(self):
-        return self.__repr__()
+        fmt = '%Y-%m-%d'
+        start = self.min.strftime(fmt)
+        end = self.max.strftime(fmt)
+        return 'TimeBubble({}, {})'.format(start, end)
 
     def __contains__(self, value):
-        return self.min <= value <= self.max
+        try:
+            return self.min <= value <= self.max
+        except:
+            return self.min <= value.min < value.max <= self.max
+
+    def to_list(self):
+        return [self.min, self.max]
 
     def merge(self, bubble):
         if self._mergeable(bubble):
             mi = min(self.min, bubble.min)
             ma = max(self.max, bubble.max)
-            return Bubble(mi, ma), None
+            return self.__class__(mi, ma), None
         else:
             return self, bubble
 
     def carve(self, bubble):
         if self._mergeable(bubble):
             l = self.min
-            u = bubble.min
-            left = Bubble(l, u) if l < u else None
-            l = bubble.max
+            u = bubble.min - timedelta(1)
+            left = self.__class__(l, u) if l < u else None
+            l = bubble.max + timedelta(1)
             u = self.max
-            right = Bubble(l, u) if l < u else None
+            right = self.__class__(l, u) if l < u else None
             return left, right
         else:
             return self, None
@@ -42,15 +57,66 @@ class Bubble:
 
 
 class Bubbles():
-    def __init__(self):
+    def __init__(self, bubbles=[]):
         self.bubbles = []
+        for b in (self._convert(b) for b in bubbles):
+            self.bubbles.append(b)
+        self._sort()
+        self._squeeze()
 
-    def append(self, bubble):
-        self.bubbles.append(bubble)
-        self.bubbles.sort(key=lambda x: x.mid)
+    def __repr__(self):
+        return 'Bubbles({})'.format(self.bubbles)
+
+    def __str__(self):
+        return self.__repr__()
+
+    def __contains__(self, value):
+        for b in self.bubbles:
+            if value in b:
+                return True
+        return False
+
+    def to_list(self):
+        res = []
+        for b in self.bubbles:
+            res.append(b.to_list())
+        return res
+
+    def merge(self, bubble):
+        self.bubbles.append(self._convert(bubble))
+        self._sort()
+        self._squeeze()
+        return self
+
+    def carve(self, bubble):
+        b_carve = self._convert(bubble)
+        res = []
+        for b in self.bubbles:
+            b1, b2 = b.carve(b_carve)
+            res.append(b1)
+            res.append(b2)
+
+        self.bubbles = res
+        self._dropnone()
+        self._sort()
+        self._squeeze()
+        return self
+
+    def _squeeze(self):
+        p = 0
         count = len(self.bubbles)
-        bubbles = [(False, bubble)]
-        for i in range(count-1):
-            l, r = self.bubbles[i].merge(self.bubbles[i+1])
-            if r is None:  # if merge success
-                pass
+        while p < count:
+            for q, b_q in ((q, b_q) for q, b_q in enumerate(self.bubbles) if q > p):
+                self.bubbles[p], self.bubbles[q] = self.bubbles[p].merge(b_q)
+            self._dropnone()
+            p += 1
+            count = len(self.bubbles)
+
+    def _sort(self):
+        self.bubbles.sort(key=lambda x: x.mid)
+
+    def _dropnone(self):
+        self.bubbles = list(filter(lambda x: not x is None, self.bubbles))
+
+    def _convert(self, limits):
+        return TimeBubble(min(limits), max(limits))
