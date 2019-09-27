d0e11ebd2ace2e6664597daa1fbcbd4b27a70869
Langford <bankszhu4869@gmail.com>
2019-09-27 11:44:09 +0800

    test

--- test.py
+++ test.py
@@ -1,22 +1,7 @@
-#import fdm
-from datetime import datetime
-from fdm.utils.data_structure import Bubbles
+import fdm
 
 
-'''edb = fdm.Wind().edb()
+edb = fdm.Wind().edb()
 # edb.interface.drop()
 data = edb.query(['M5567876', 'M5567877'], autoupdate=False)
-print(data.pivot('date', 'code', 'value'))'''
-
-
-start = datetime.now()
-l = [[datetime(2000, 1, 1), datetime(2000, 2, 1)],
-     [datetime(2000, 3, 1), datetime(2000, 5, 30)],
-     [datetime(2000, 6, 1), datetime(2000, 12, 1)]
-     ]
-
-b0 = Bubbles(l)
-bs = b0.gaps()
-
-print(b0.to_actualrange())
-print(bs.to_actualrange())
+print(data.pivot('date', 'code', 'value'))
