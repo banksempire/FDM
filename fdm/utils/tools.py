5befe40f06d33a2a02c137db12c2501468c7e2dd
Langford <bankszhu4869@gmail.com>
2019-10-24 15:18:38 +0800

    ADD test_feeder_func

--- fdm/utils/tools.py
+++ fdm/utils/tools.py
@@ -9,3 +9,16 @@ def prev_date(date: datetime, freq: str) -> datetime:
 
 def next_date(date: datetime, freq: str) -> datetime:
     return pd.date_range(start=date, periods=2, freq=freq)[1].to_pydatetime()
+
+
+def test_feeder_func(code, field, start, end):
+    def gen():
+        for i in pd.date_range(start, end):
+            value = code + field + i.strftime('%Y%m%d')
+            doc = {
+                'date': i,
+                'code': code,
+                field: value,
+            }
+            yield pd.Series(doc)
+    return pd.DataFrame(gen())
--- test.py
+++ test.py
@@ -3,9 +3,13 @@ from datetime import datetime
 from fdm.datasources.metaclass.manager import Manager
 from fdm.utils.client import client
 
-tdb = client['test']['test']
+from fdm.utils.tools import test_feeder_func
+
+'''tdb = client['test']['test']
 fm = Manager(tdb)
 
 for i in fm.solve_update_params('abc', 'cde', datetime(2000, 1, 1), datetime(2012, 1, 1)):
     print(i)
-client.close()
+client.close()'''
+
+print(test_feeder_func('abc', 'cdb', datetime(2019, 1, 1), datetime(2019, 2, 1)))
