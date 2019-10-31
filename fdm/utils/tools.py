321ea2ddeb63c272d58800433e47bae9b1ce2eb8
zlf <zhulf4869@outlook.com>
2019-10-31 22:13:51 +0800

    ADD del_id

--- fdm/utils/tools.py
+++ fdm/utils/tools.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 import pandas as pd
+from pandas import DataFrame
 
 
 def prev_date(date: datetime, freq: str) -> datetime:
@@ -9,3 +10,10 @@ def prev_date(date: datetime, freq: str) -> datetime:
 
 def next_date(date: datetime, freq: str) -> datetime:
     return pd.date_range(start=date, periods=2, freq=freq)[1].to_pydatetime()
+
+
+def del_id(df) -> DataFrame:
+    '''Delete column['_id'] from result'''
+    if not df.empty:
+        del df['_id']
+    return df
