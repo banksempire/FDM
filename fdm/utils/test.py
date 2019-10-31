721367d760c46da90c89f72850b8475c5f4be749
zlf <zhulf4869@outlook.com>
2019-10-31 20:31:19 +0800

    MOD change freq ro Q

--- fdm/utils/test.py
+++ fdm/utils/test.py
@@ -16,9 +16,9 @@ def test_feeder_func(code, field, start, end):
     return pd.DataFrame(gen())
 
 
-def test_feeder_func_W(code, field, start, end):
+def test_feeder_func_Q(code, field, start, end):
     def gen():
-        for i in pd.date_range(start, end, freq='W'):
+        for i in pd.date_range(start, end, freq='Q'):
             value = code + field + i.strftime('%Y%m%d')
             doc = {
                 'date': i,
