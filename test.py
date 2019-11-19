4b0cfeb43f42f4227183aaf93c2175d78b42d369
zlf <zhulf4869@outlook.com>
2019-11-19 23:27:25 +0800

    Reorg test

--- test.py
+++ test.py
@@ -8,25 +8,37 @@ from fdm.datasources.metaclass.interface import StaColInterface
 from fdm.datasources.metaclass.manager import FieldStatus
 
 
-client.drop_database('test')
-time = datetime.now()
-interface = StaColInterface(
-    client['test']['test'], ord_test_feeder_func, test_config['Test']['DBSetting'])
+def test_one_dummy():
+    client.drop_database('test')
+    time = datetime.now()
+    interface = StaColInterface(
+        client['test']['test'], ord_test_feeder_func, test_config['Test']['DBSetting'])
+    print(interface.query(['abc'], fields=['cdb'], startdate=datetime(
+        1990, 1, 1), enddate=datetime(2020, 12, 1)))
+    print(datetime.now()-time)
 
 
-print(interface.query(['abc'], fields=['cdb'], startdate=datetime(
-    1990, 1, 1), enddate=datetime(2020, 12, 1)))
+def multi_dummy():
+    client.drop_database('test')
+    time = datetime.now()
+    interface = StaColInterface(
+        client['test']['test'], ord_test_feeder_func, test_config['Test']['DBSetting'])
+    codes = ['code_' + str(i) for i in range(20)]
+    print(codes)
+    print(interface.query(codes, fields=['cdb'], startdate=datetime(
+        1990, 1, 1), enddate=datetime(2019, 4, 1)))
+    print(datetime.now()-time)
 
-'''codes = ['code_' + str(i) for i in range(20)]
-print(codes)
-print(interface.query(codes, fields=['cdb'], startdate=datetime(
-    2018, 1, 1), enddate = datetime(2019, 4, 1))) '''
-'''codes = '000001.SZ,000002.SZ,000004.SZ,000005.SZ,000006.SZ,000007.SZ,000008.SZ,000009.SZ,000010.SZ,000011.SZ'.split(
-    ',')
-wsd = fdm.Wind().wsd()
-time = datetime.now()
-print(wsd.query(codes, fields='close', startdate=datetime(
-    2018, 1, 1), enddate=datetime(2019, 4, 1)))
 
-print(datetime.now()-time)'''
+def test_wind():
+    codes = '000001.SZ,000002.SZ,000004.SZ,000005.SZ,000006.SZ,000007.SZ,000008.SZ,000009.SZ,000010.SZ,000011.SZ'.split(
+        ',')
+    wsd = fdm.Wind().wsd()
+    time = datetime.now()
+    print(wsd.query(codes, fields='close', startdate=datetime(
+        2018, 1, 1), enddate=datetime(2019, 4, 1)))
+    print(datetime.now()-time)
+
+
+multi_dummy()
 client.close()
