03fe8cf1c19a5602c1c1184d3641924aad4693b3
banksempire <bankszhu4869@gmail.com>
2019-11-24 16:14:59 +0800

    ADD Tushare daily_basic

--- fdm/datasources/tushare/feeder.py
+++ fdm/datasources/tushare/feeder.py
@@ -99,3 +99,34 @@ def adj_factor(cls, code: str, field: str, start: datetime, end: datetime):
     if data.shape[1] == 2:
         del tushare_cache[cache_name, code]
     return res
+
+
+def daily_basic(cls, code: str, field: str, start: datetime, end: datetime):
+    '''
+    Tushare other daily trading info.
+    '''
+    @retry(10)
+    def downloader(code, start, end):
+        import tushare as ts
+        pro = ts.pro_api()
+        df = pro.daily_basic(ts_code=code,
+                             start_date=start.strftime(
+                                 '%Y%m%d'),
+                             end_date=end.strftime('%Y%m%d'))
+        return df
+
+    cache_name = 'daily_basic'
+    if tushare_cache[cache_name, code].empty:
+        tushare_cache[cache_name, code] = downloader(
+            code, start, end)
+
+    data = tushare_cache[cache_name, code]
+    res = data[['ts_code',
+                'trade_date',
+                field]].copy()
+
+    del data[field]
+    # delete from cache if all data has been returned
+    if data.shape[1] == 2:
+        del tushare_cache[cache_name, code]
+    return res
