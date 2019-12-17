0a81f9b67c1a950b9fa09d0d1f70c8412185e3b6
banksempire <bankszhu4869@gmail.com>
2019-12-17 14:03:28 +0800

    update fields

--- fdm/datasources/joinquant/fields.py
+++ fdm/datasources/joinquant/fields.py
@@ -5,12 +5,6 @@ price_fields = [
     'low',
     'volume',
     'money',
-    'open||post',
-    'close||post',
-    'high||post',
-    'low||post',
-    'volume||post',
-    'money||post',
 ]
 
 fs_fields = {
